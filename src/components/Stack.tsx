66725db83913b3f58f314656d36c0180a70606dc
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-01-13 15:31:40 -0800

    feat(stack): adding relative property as test

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -2,14 +2,15 @@ import { CSSProperties } from "react";
 import { styled } from "styled-components";
 
 type StackProps = {
-  flexDirection?: CSSProperties['flexDirection'];
-  alignSelf?: CSSProperties['alignSelf'];
-}
+  flexDirection?: CSSProperties["flexDirection"];
+  alignSelf?: CSSProperties["alignSelf"];
+};
 
 const Stack = styled.div<StackProps>((props) => ({
   flexDirection: props.flexDirection ?? "column",
   alignSelf: props.alignSelf ?? "inherit",
   display: "flex",
-}))
+  position: "relative",
+}));
 
-export default Stack
+export default Stack;
